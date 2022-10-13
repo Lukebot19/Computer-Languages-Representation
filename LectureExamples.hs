873763273e9c6e4d26bd6dfc6228dc48a0c49ae7
Lukebot19 <lukealexander2a@gmail.com>
2022-10-13 13:51:18 +0100

    lots of ones

--- LectureExamples.hs
+++ LectureExamples.hs
@@ -6,6 +6,13 @@ productOf :: [ Int ] -> Int
 productOf xs
     = foldr multiply 1 xs
 
-main:: IO ()
-main = putStrLn( show( productOf [1,2,3,4,5]))
+-- main:: IO ()
+-- main = putStrLn( show( productOf [1,2,3,4,5]))
+
+
+ones :: [Int]
+ones = 1 : ones
+
+main :: IO ()
+main = putStrLn( show( take 10000000 ones))
 
