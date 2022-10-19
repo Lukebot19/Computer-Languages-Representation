f5ffc0e7d74b27cdd54fc8a05b1c8abf9c387af3
Lukebot19 <lukealexander2a@gmail.com>
2022-10-19 11:04:43 +0100

    Update LectureExamples.hs
    Lambda

--- LectureExamples.hs
+++ LectureExamples.hs
@@ -46,5 +46,43 @@ isPerfect n
 perfects :: [Int]
 perfects = consumer producer
 
-main :: IO ()
-main = putStrLn( show ( take 5 perfects ))
+
+-- main :: IO ()
+-- main = putStrLn( show ( take 5 perfects ))
+
+-- Write and read a text file
+test :: String -> String -> IO String
+test fn s = writeFile fn s >> readFile fn
+
+-- main :: IO ()
+-- main = test "a.txt" "a" >>= putStrLn
+
+
+-- It broke
+-- test1 :: String -> String -> IO String
+-- test1 fn s 
+--     = do writeFile fn s 
+--     readFile fn
+
+-- main :: IO ()
+-- main 
+--     = do s <- test1 "a.txt" "a" 
+--     putStrLn s
+
+-- Lambda functions
+lambda :: Int -> Int -> Int
+lambda = \x y -> x + y
+
+-- main :: IO ()
+-- main = putStrLn( show ( lambda 42 1))
+
+lambda2 :: Int -> Bool
+lambda2 = \x -> x > 7
+
+test2 :: [Int]
+test2 = filter lambda2 [1..9]
+
+main:: IO ()
+main = putStrLn ( show test2)
+
+
